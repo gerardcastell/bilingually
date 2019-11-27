b02b5867e20340fef3cd23cfdffa7fca5681e1cf
Gerard <gerardcastell97@gmail.com>
2019-11-27 12:08:20 +0100

    Updates

---
+++ precache-manifest.8e680b2277b3d8899a350814471bfbd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bc85fefd208b0f29eff13152875c26",
+    "url": "/bilingually/index.html"
+  },
+  {
+    "revision": "ad4a837e5d0d3b87d1f1",
+    "url": "/bilingually/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3f56f20ab7b71c80e616",
+    "url": "/bilingually/static/js/2.7ac73cba.chunk.js"
+  },
+  {
+    "revision": "ad4a837e5d0d3b87d1f1",
+    "url": "/bilingually/static/js/main.61fc76b2.chunk.js"
+  },
+  {
+    "revision": "7a2bd27d3617e71a44ae",
+    "url": "/bilingually/static/js/runtime-main.48057b9b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bilingually/static/media/logo.25bf045c.svg"
+  }
+]);
